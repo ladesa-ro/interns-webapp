2b2cb71d257c1e58bd6406bd591e08cccf33f079
Victor Dev <victorcardosoferreirahenrique@gmail.com>
2026-04-10 12:10:56 -0400

    refactor:MENU e PAINEL CIEC :hammer:
    fiz o menu e comecei a fazer o pinel ciec

--- Ladesa-Estagios/src/App.jsx
+++ Ladesa-Estagios/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Layout from "./components/Layout";
 
 function App() {
-  
+  return <Layout />;
 }
 
-export default App
+export default App;
