93e5e444a18fd93b5ad1d733cfd571b056a16c24
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-04-24 13:45:12 -0400

    feat: add login screen  :sparkles:

--- Ladesa-Estagios/src/App.jsx
+++ Ladesa-Estagios/src/App.jsx
@@ -1,7 +1,13 @@
-import Layout from "./components/Layout";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import AppRoutes from "./routes";
 
 function App() {
-  return <Layout />;
+  return(
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- Ladesa-Estagios/src/pages/Login.jsx
+++ Ladesa-Estagios/src/pages/Login.jsx
@@ -1,13 +1,60 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
+import { useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 
-const Login = () => {
-return (
-    <div>
-        <h1>abc</h1>
-        <p>abc</p>
-    </div>
-);
-};
+export default function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    senha: "",
+  });
 
-export default Login;
+  const navigate = useNavigate();
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setForm({ ...form, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // 🔥 simulação de login
+    if (form.email === "admin@gmail.com" && form.senha === "123") {
+      const fakeToken = "abc123";
+
+      localStorage.setItem("token", fakeToken);
+
+      navigate("/painel");
+    } else {
+      alert("Login inválido");
+    }
+  }
+
+  // 🔒 impede acessar login estando logado
+  if (localStorage.getItem("token")) {
+    return <Navigate to="/painel" />;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={handleChange}
+      />
+
+      <input
+        type="password"
+        name="senha"
+        placeholder="Senha"
+        value={form.senha}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
