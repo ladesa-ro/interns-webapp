0f3a0a42e33c2b98b35b9fb2423c9186c2a84021
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-04-28 23:50:12 -0400

    feat: finishing the inputPassword

---
+++ Ladesa-Estagios/src/components/icons_Components/Icon_EyeOpen_Comp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import IconEyeOpenSvg from '../../assets/icons/icon_EyeOpen.svg?react';
+
+const IconEyeOpen = ({ width = "30", height = "30", className = "" }) => {
+  return (
+    <IconEyeOpenSvg width={width} height={height} className={className} />
+  );
+};
+
+export default IconEyeOpen;
