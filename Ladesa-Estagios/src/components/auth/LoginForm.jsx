9ca5bdc49fd1b1f2fbba031d584471f89c57891c
UrielAndrade <andradeleiru@gmail.com>
2026-04-29 12:53:51 -0400

    feat: add LoginForm component for user authentication

---
+++ Ladesa-Estagios/src/components/auth/LoginForm.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./LoginForm.module.css";
+import Titulo from "../icons_Components/Icon_Logo_Comp";
+import Imagem from "../image_Components/Image_Login_Comp";
+import { Eye, EyeOff } from "lucide-react";
+
+export default function LoginForm({ form, onChange, onSubmit }) {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.card}>
+          <form onSubmit={onSubmit} className={styles.form}>
+            <Titulo className={styles.logo} />
+            <input
+              type="text"
+              name="matricula"
+              placeholder="Insira seu usuário"
+              value={form.matricula}
+              onChange={onChange}
+            />
+
+            <div className={styles.inputPassWord}>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="senha"
+                placeholder="Insira sua senha"
+                value={form.senha}
+                onChange={onChange}
+              />
+              <span
+                className={styles.iconEye}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <Eye /> : <EyeOff />}
+              </span>
+            </div>
+
+            <p>
+              Esqueceu a senha? <a href="/">Clique aqui.</a>
+            </p>
+
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        <Imagem />
+      </div>
+    </div>
+  );
+}
