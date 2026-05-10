0d1ce3fb12b48f9bc79198981a42b4b35df2df0b
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-05-10 11:11:48 -0400

    feat: updating the tabelaComponent  :sparkles:

--- src/components/global_Components/Tabela.jsx
+++ src/components/global_Components/Tabela.jsx
@@ -1,49 +1,26 @@
 import React from 'react';
 import styles from "./Tabela.module.css";
-const empresas = [{
-    nome: "Tech Solutions",
-    curso: "Informática",
-    contato: "techsolutions@gmail.com"
-},
-{
-    nome: "Inova Digital",
-    curso: "Informática",
-    contato: "inovadigital@gmail.com"
-},
-{
-    nome: "Laboratório BioVida",
-    curso: "Química",
-    contato: "biovida@gmail.com"
-},
-{
-    nome: "QuimLab Análise",
-    curso: "Química",
-    contato: "quimlab@gmail.com"
-},
-{
-    nome: "Floresta Viva",
-    curso: "Floresta",
-    contato: "floresta@gmail.com"
-}
 
-];
-const Tabela = () => {
+const Tabela = ({colunas,dados}) => {
   return (
     <div className={styles.tabela}>
         <table>
             <thead>
                 <tr className={styles.titulo}>
-                    <th>Nome da Empresa</th>
-                    <th>Curso</th>
-                    <th>Contato</th>
+                   {colunas.map((coluna) => (
+                    <th key={coluna.chave}>
+                        {coluna.label}
+                        </th>))}
                 </tr>
             </thead>
             <tbody>
-                {empresas.map((empresa, index) => (
-                    <tr key={index} className={styles.linha}>
-                        <td>{empresa.nome}</td>
-                        <td>{empresa.curso}</td>
-                        <td>{empresa.contato}</td>
+                {dados.map((item) => (
+                    <tr key={item.id} className={styles.linha}>
+                        {colunas.map((coluna) => (
+                            <td key={coluna.chave}>
+                                {item[coluna.chave]}
+                            </td>
+                        ))}
                     </tr>
                 ))}
             </tbody>
