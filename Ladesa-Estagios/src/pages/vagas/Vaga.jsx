3493a16783e26b7b969789d6c89e1a44bb8a6626
Victor Dev <victorcardosoferreirahenrique@gmail.com>
2026-05-11 09:28:23 -0400

    refactor:Painel ciec rolagem dos cards  :hammer:
    quando a tela painel ficar mais pequena aparece uma seta pra mostrar os outros cards

--- Ladesa-Estagios/src/pages/vagas/Vaga.jsx
+++ Ladesa-Estagios/src/pages/vagas/Vaga.jsx
@@ -1,3 +1,38 @@
+import "./vaga.css";
+import "../../styles/global.css";
 export default function Vaga() {
-  return <h1>Cadastrar Vaga</h1>;
+  return (
+    <div className="vaga">
+      <div className="content">
+        <h2 className="titulo">← Painel CIEC</h2>
+        <p className="sub">Vagas Disponíveis</p>
+
+        <div className="cards">
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1518770660439-4636190af475" />
+            <div className="card-body">
+              <h3>Informática</h3>
+              <span>24</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="https://static.todamateria.com.br/upload/fi/si/fisicoquimicacke1.jpg" />
+            <div className="card-body">
+              <h3>Química</h3>
+              <span>24</span>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470" />
+            <div className="card-body">
+              <h3>Florestas</h3>
+              <span>24</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
