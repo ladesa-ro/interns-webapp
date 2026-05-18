f0285933dd6c2af894195dd29b3f3717512645d7
Victor Dev <victorcardosoferreirahenrique@gmail.com>
2026-05-18 19:45:04 -0400

    refactor: tela Painel CIEC >> Alunos em estagios  :hammer:
    desenvolvir a tela de alunos em estagio

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
+
 import Painel from "../../pages/dashboard/Painel";
 import Empresa from "../../pages/empresas/Empresa";
 import Vaga from "../../pages/vagas/Vaga";
 import ListaEspera from "../../pages/lista-espera/ListaEspera";
 import Perfil from "../../pages/perfil/Perfil";
-
+import AlunosEmEstagio from "../../pages/alunos-em-estagios/Alunos-em-estagio";
 export default function Layout() {
   return (
     <div className="app">
@@ -14,10 +15,19 @@ export default function Layout() {
       <div className="content">
         <Routes>
           <Route index element={<Painel />} />
+
           <Route path="empresa" element={<Empresa />} />
+
           <Route path="vaga" element={<Vaga />} />
+
           <Route path="lista" element={<ListaEspera />} />
+
           <Route path="perfil" element={<Perfil />} />
+
+          <Route
+            path="alunos-em-estagio"
+            element={<AlunosEmEstagio />}
+          />
         </Routes>
       </div>
     </div>
