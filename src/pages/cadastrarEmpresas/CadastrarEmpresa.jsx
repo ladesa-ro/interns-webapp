ed1feab2a8112e89679175001ad346515bb9f4fe
Braga <Bragapensador@gmail.com>
2026-06-03 14:43:15 -0400

    feat: starting creating cadastrarEmpresaScreen  :sparkles:

---
+++ src/pages/cadastrarEmpresas/CadastrarEmpresa.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from '../cadastrarEmpresas/cadastrarEmpresa.module.css';
+
+export default function CadastrarEmpresa() {
+  return (
+    <div className={Styles.container}>
+      <h1>Cadastrar Empresa</h1>
+    </div>
+  );
+}
