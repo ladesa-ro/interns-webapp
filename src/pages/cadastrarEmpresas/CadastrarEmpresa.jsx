c88e1addb40dd722928f547cd17f3d6a485be10a
Braga <Bragapensador@gmail.com>
2026-06-03 15:20:15 -0400

    feat: adding more elements in the topScreen  :sparkles:

--- src/pages/cadastrarEmpresas/CadastrarEmpresa.jsx
+++ src/pages/cadastrarEmpresas/CadastrarEmpresa.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Styles from '../cadastrarEmpresas/cadastrarEmpresa.module.css';
+import {Plus} from 'lucide-react';
 
 export default function CadastrarEmpresa() {
   return (
     <div className={Styles.container}>
-      <h1>Cadastrar Empresa</h1>
+      <div className={Styles.topContainer}>
+      <div>
+      <h1>Cadastro de Empresas</h1>
+      <h3>Gerencie as empresas parceiras do IFRO</h3>
+      </div>
+      <button className={Styles.botaoCadastrar}>
+        <div><Plus/> Nova Empresa</div>
+      </button>
+    </div>
     </div>
   );
 }
