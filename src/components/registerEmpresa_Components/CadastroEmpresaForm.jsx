8f78e05f840cc7f69a68aa6e1753131cb246327a
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-06-09 12:16:59 -0400

    feat: styling the novaEmpresa Screen :sparkles:

--- src/components/registerEmpresa_Components/CadastroEmpresaForm.jsx
+++ src/components/registerEmpresa_Components/CadastroEmpresaForm.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Styles from '../../components/registerEmpresa_Components/cadastroEmpresaForm.module.css';
 export default function CadastroEmpresaForm() {
+
+  const navigate = useNavigate();
+
   return (
      <div>
+      {/*icone aqui*/}
       <h2>Cadastrar Nova Empresa</h2>
 
       <form>
-        <input placeholder="Matrícula" />
-        <input placeholder="CNPJ" />
-
         <input placeholder="Razão Social" />
         <input placeholder="Nome Fantasia" />
 
-        <input placeholder="Responsável" />
+        <input placeholder="00.000.000/0000-00" />
         <input placeholder="E-mail" />
-
         <input placeholder="Telefone" />
-        <input placeholder="Endereço" />
 
         <input placeholder="Cidade" />
         <input placeholder="Estado" />
 
-        <button type="submit">
-          Cadastrar Empresa
-        </button>
+        <div className={Styles.botoes}>
+          <button
+            type="button"
+            className={Styles.botaoCancelar}
+            onClick={() => navigate('/cadastrarempresa')}
+          >
+            Cancelar
+          </button>
+
+          <button
+            type="submit"
+            className={Styles.botaoCadastrar}
+          >
+            Cadastrar Empresa
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/pages/novaEmpresa/NovaEmpresa.jsx
+++ src/pages/novaEmpresa/NovaEmpresa.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import CadastroEmpresaForm from '../../components/registerEmpresa_Components/CadastroEmpresaForm';
 
 export default function NovaEmpresa() {
