1013888ff2dd1e1c01766456eb387354dc66be60
Victor Dev <victorcardosoferreirahenrique@gmail.com>
2026-06-10 09:48:58 -0400

    refactor: telas dos alunos >> inicio >> Solicitar estagio :hammer:
    soliciatar estagio externo e interno falta colocar os input certo em interno

--- src/components/layout/LayoutAluno.jsx
+++ src/components/layout/LayoutAluno.jsx
@@ -5,7 +5,7 @@ import Inicio from "../../pages/aluno/inicio/Inicio";
 import Perfil from "../../pages/aluno/Perfil";
 import ListaEsperaAluno from "../../pages/aluno/ListaEsperaAluno";
 import GuiaEstagio from "../../pages/aluno/GuiaEstagio";
-
+import SolicitarEstagio from "../../pages/aluno/inicio/solicitar-estagio/SolicitarEstagio";
 export default function LayoutAluno() {
   return (
     <div className="app">
@@ -17,6 +17,10 @@ export default function LayoutAluno() {
           <Route path="perfil" element={<Perfil />} />
           <Route path="lista-espera" element={<ListaEsperaAluno />} />
           <Route path="guia-estagio" element={<GuiaEstagio />} />
+          <Route
+            path="solicitar-estagio"
+            element={<SolicitarEstagio />}
+          />
         </Routes>
       </div>
     </div>
